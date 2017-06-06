28268fca6382d36aee74470f74bbab7cfa0e1d28
GabrielaLombardi <gabriela.lombardi.barrios@gmail.com>
2017-06-06 12:16:08 -0600

    Actividad 1
    Es aquí donde se calcula la hipotenusa del triangulo rectangulo

---
+++ HT1_201503529/src/mainclass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cynthia
+ */
+public class mainclass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaUnica ventana = new VentanaUnica();
+        ventana.setVisible(true);
+    }
+    
+}
